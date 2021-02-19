53ba3be324bdde6cf5265d97e2c5ed2a71616d11
Victor Hugo Monteiro de Almeida <45021850+Mashslek@users.noreply.github.com>
2021-02-19 17:28:23 -0300

    Otimização

--- Agariando Fundos/Program.cs
+++ Agariando Fundos/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ConsoleApp5
 {
@@ -11,12 +12,12 @@ namespace ConsoleApp5
         static void Main(string[] args)
         {
             List<Guest> Guests = new List<Guest>();
-            int inputs = Int32.Parse(Console.ReadLine());
-
-            int[,] donateMatrix = new int[inputs,3];
+            
+            StreamReader testFile = new StreamReader("D:\\InputFilePath");
+            int inputs = Int32.Parse(testFile.ReadLine());
             for (int x = 0; x < inputs; x++)
             {
-                string[] line = Console.ReadLine().Split(' ');
+                string[] line = testFile.ReadLine().Split(' ');
                 Guests.Add(new Guest
                 {
                     guestId = x,
@@ -25,6 +26,15 @@ namespace ConsoleApp5
                     donation = Int32.Parse(line[2])
                 });
             }
+            Guests = Guests.GroupBy(x => x.richesness.ToString() + "|" + x.beauty)
+                          .Select((x, index) => {
+                              return new Guest {
+                                  guestId = index,
+                                  donation = x.Sum(y => y.donation),
+                                  beauty = x.First().beauty,
+                                  richesness = x.First().richesness,
+                              };
+                          }).ToList();
             Guests.ForEach(x =>
             {
                 x.guestRestriction = Guests.Where(y => (x.beauty < y.beauty && x.richesness >= y.richesness) ||
@@ -33,21 +43,37 @@ namespace ConsoleApp5
                                                        (y.beauty >= x.beauty && y.richesness < x.richesness)).Select(y => y.guestId).ToList();
                // Console.WriteLine("ID : " + x.guestId + "\n Beleza : " + x.beauty + "\n Riqueza : " + x.richesness + "\n Doação : " + x.donation + "\n Restricao : " + String.Join("|",x.guestRestriction) + "__________________________________");
             });
-            Console.WriteLine(Guests.Select(x => getBestDonationByFixedGuest(x, Guests)).Max());
+            
+            int solution = Guests.Select(x => getBestDonationByFixedGuest(x, Guests.OrderBy(y => y.guestId).ToList())).Max();
+            Console.WriteLine(solution);
+            StreamWriter streamWriter = new StreamWriter("D:\\OutputFilePath");            
+            streamWriter.WriteLine("Resposta : " + solution);
+            streamWriter.WriteLine(String.Join("\n", calculatedSubTrees.Select(x => String.Join("|", x.Key+ "\t\t" + x.Value))));
+            streamWriter.Flush();
         }
         static int getBestDonationByFixedGuest(Guest curr, List<Guest> possibleGests) {
+
             possibleGests = possibleGests.Where(x => !curr.guestRestriction.Contains(x.guestId)).ToList();
+            int subTreeMax;
             List<int> possibleDonations = new List<int>();
+
             if (possibleGests.Count() == 1)
                 return curr.donation;
 
             possibleGests.Remove(curr);
-            for (int x = 0;x < possibleGests.Count(); x++)
+
+            if (calculatedSubTrees.TryGetValue(String.Join("|", possibleGests.Select(x => x.guestId.ToString())), out subTreeMax))
             {
-                possibleDonations.Add(getBestDonationByFixedGuest(possibleGests[x], possibleGests));
+                return curr.donation + subTreeMax;
             }
+
+            for (int x = 0;x < possibleGests.Count(); x++)
+                possibleDonations.Add(getBestDonationByFixedGuest(possibleGests[x], possibleGests));
+
+            calculatedSubTrees.Add(String.Join("|",possibleGests.Select(x=>x.guestId.ToString())), possibleDonations.Max());
             return curr.donation + possibleDonations.Max();
         }
+        static Dictionary<string, int> calculatedSubTrees = new Dictionary<string, int>();
     }
     class Guest {
         public List<int> guestRestriction { get; set; }
